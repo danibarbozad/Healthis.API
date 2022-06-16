6b1d5846fcb92d236254ed6334eae840ad198699
Daniel Barboza <danibarbozad@gmail.com>
2022-06-16 13:34:10 -0300

    create services layer + address service

---
+++ Healthis.Service/EnderecoService.cs
@@ -0,0 +1,25 @@
+﻿using Healthis.Entities;
+using Healthis.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Healthis.Service
+{
+    public class EnderecoService
+    {
+        private EnderecoModel enderecoModel;
+        public EnderecoService(string connectionString)
+        {
+            enderecoModel = new EnderecoModel(connectionString);
+        }
+
+        public Endereco Create(Endereco endereco) => enderecoModel.Create(endereco);
+        public Endereco Update(Endereco endereco) => enderecoModel.Update(endereco);
+        public bool Delete(int enderecoID) => enderecoModel.Delete(enderecoID);
+        public List<Endereco> GetAll => enderecoModel.GetAll();
+        public Endereco Get(int enderecoID) => enderecoModel.Get(enderecoID);
+    }
+}
