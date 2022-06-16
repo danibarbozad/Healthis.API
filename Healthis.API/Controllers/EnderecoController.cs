18ba3cc364d98d6acf49463ffea46804783aceaf
Daniel Barboza <danibarbozad@gmail.com>
2022-06-16 14:12:09 -0300

    create address controller

---
+++ Healthis.API/Controllers/EnderecoController.cs
@@ -0,0 +1,53 @@
+﻿using Healthis.Entities;
+using Healthis.Service;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Healthis.API.Controllers
+{
+    public class EnderecoController : ApiController
+    {
+        [HttpGet]
+        public List<Endereco> Get()
+        {
+            EnderecoService service = new EnderecoService(ConfigurationManager.ConnectionStrings["HealthisDB"].ConnectionString);
+            return service.GetAll();
+        }
+
+        [HttpGet]
+        public Endereco Get(int id)
+        {
+            EnderecoService service = new EnderecoService(ConfigurationManager.ConnectionStrings["HealthisDB"].ConnectionString);
+            return service.Get(id);
+        }
+
+        [HttpPost]
+        [Route("api/endereco/create")]
+        public Endereco Create([FromBody] Endereco endereco)
+        {
+            EnderecoService service = new EnderecoService(ConfigurationManager.ConnectionStrings["HealthisDB"].ConnectionString);
+            return service.Create(endereco);
+        }
+
+        [HttpPost]
+        [Route("api/endereco/update")]
+        public Endereco Update([FromBody] Endereco endereco)
+        {
+            EnderecoService service = new EnderecoService(ConfigurationManager.ConnectionStrings["HealthisDB"].ConnectionString);
+            return service.Update(endereco);
+        }
+
+        [HttpPost]
+        [Route("api/endereco/delete/{id}")]
+        public bool Delete(int id)
+        {
+            EnderecoService service = new EnderecoService(ConfigurationManager.ConnectionStrings["HealthisDB"].ConnectionString);
+            return service.Delete(id);
+        }
+    }
+}
