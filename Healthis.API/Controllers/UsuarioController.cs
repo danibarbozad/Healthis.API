add98c9e5c1d6cbf379fe2ec58bf4253a322baf8
Daniel Barboza <danibarbozad@gmail.com>
2022-06-16 14:12:33 -0300

    create user controller (without authentication temporarily)

---
+++ Healthis.API/Controllers/UsuarioController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Healthis.Entities;
+using Healthis.Service;
+using System.Configuration;
+
+namespace Healthis.API.Controllers
+{
+    public class UsuarioController : ApiController
+    {
+        [HttpGet]
+        public List<Usuario> Get()
+        {
+            UsuarioService service = new UsuarioService(ConfigurationManager.ConnectionStrings["HealthisDB"].ConnectionString);
+            return service.GetAll();
+        }
+
+        [HttpGet]
+        public Usuario Get(int id)
+        {
+            UsuarioService service = new UsuarioService(ConfigurationManager.ConnectionStrings["HealthisDB"].ConnectionString);
+            return service.Get(id);
+        }
+
+        [HttpPost]
+        [Route("api/usuario/create")]
+        public Usuario Create([FromBody] Usuario usuario)
+        {
+            UsuarioService service = new UsuarioService(ConfigurationManager.ConnectionStrings["HealthisDB"].ConnectionString);
+            return service.Create(usuario);
+        }
+
+        [HttpPost]
+        [Route("api/usuario/update")]
+        public Usuario Update([FromBody] Usuario usuario)
+        {
+            UsuarioService service = new UsuarioService(ConfigurationManager.ConnectionStrings["HealthisDB"].ConnectionString);
+            return service.Update(usuario);
+        }
+
+        [HttpPost]
+        [Route("api/usuario/delete/{id}")]
+        public bool Delete(int id)
+        {
+            UsuarioService service = new UsuarioService(ConfigurationManager.ConnectionStrings["HealthisDB"].ConnectionString);
+            return service.Delete(id);
+        }
+    }
+}
