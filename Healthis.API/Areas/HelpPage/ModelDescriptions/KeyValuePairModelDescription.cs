bd4562615786ddcf7bc6000425ee8bf3aca5ae17
Daniel Barboza <danibarbozad@gmail.com>
2022-06-15 21:41:29 -0300

    Add project files.

---
+++ Healthis.API/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Healthis.API.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
