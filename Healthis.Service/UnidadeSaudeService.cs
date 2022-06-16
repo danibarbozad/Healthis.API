527f66fee7bfb2c55e1bb4cf178c7ad556039abd
Daniel Barboza <danibarbozad@gmail.com>
2022-06-16 13:39:02 -0300

    create health unity service

--- Healthis.Service/UnidadeSaudeService.cs
+++ Healthis.Service/UnidadeSaudeService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Healthis.Entities;
+using Healthis.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,18 @@ using System.Threading.Tasks;
 
 namespace Healthis.Service
 {
-    class UnidadeSaudeService
+    public class UnidadeSaudeService
     {
+        private UnidadeSaudeModel unidadeSaudeModel;
+        public UnidadeSaudeService(string connectionString)
+        {
+            unidadeSaudeModel = new UnidadeSaudeModel(connectionString);
+        }
+
+        public UnidadeSaude Create(UnidadeSaude unidadeSaude) => unidadeSaudeModel.Create(unidadeSaude);
+        public UnidadeSaude Update(UnidadeSaude unidadeSaude) => unidadeSaudeModel.Update(unidadeSaude);
+        public bool Delete(int unidadeSaudeID) => unidadeSaudeModel.Delete(unidadeSaudeID);
+        public List<UnidadeSaude> GetAll => unidadeSaudeModel.GetAll();
+        public UnidadeSaude Get(int unidadeSaudeID) => unidadeSaudeModel.Get(unidadeSaudeID);
     }
 }
