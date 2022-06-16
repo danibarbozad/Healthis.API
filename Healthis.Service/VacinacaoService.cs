0de2c43586f24b7e4f20b6cb7894f9d780f1ab7f
Daniel Barboza <danibarbozad@gmail.com>
2022-06-16 13:39:36 -0300

    create vaccination service

--- Healthis.Service/VacinacaoService.cs
+++ Healthis.Service/VacinacaoService.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Healthis.Entities;
+using Healthis.Model;
 
 namespace Healthis.Service
 {
-    class VacinacaoService
+    public class VacinacaoService
     {
+        private VacinacaoModel vacinacaoModel;
+        public VacinacaoService(string connectionString)
+        {
+            vacinacaoModel = new VacinacaoModel(connectionString);
+        }
+
+        public Vacinacao Create(Vacinacao vacinacao) => vacinacaoModel.Create(vacinacao);
+        public Vacinacao Update(Vacinacao vacinacao) => vacinacaoModel.Update(vacinacao);
+        public bool Delete(int vacinacaoID) => vacinacaoModel.Delete(vacinacaoID);
+        public List<Vacinacao> GetAll => vacinacaoModel.GetAll();
+        public Vacinacao Get(int vacinacaoID) => vacinacaoModel.Get(vacinacaoID);
     }
 }
