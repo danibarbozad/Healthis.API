27f7857fc56c2b0d238925cd47a49887ff1542ee
Daniel Barboza <danibarbozad@gmail.com>
2022-06-16 13:39:44 -0300

    create vaccines service

--- Healthis.Service/VacinaService.cs
+++ Healthis.Service/VacinaService.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Healthis.Entities;
+using Healthis.Model;
 
 namespace Healthis.Service
 {
-    class VacinaService
+    public class VacinaService
     {
+        private VacinaModel vacinaModel;
+        public VacinaService(string connectionString)
+        {
+            vacinaModel = new VacinaModel(connectionString);
+        }
+
+        public Vacina Create(Vacina vacina) => vacinaModel.Create(vacina);
+        public Vacina Update(Vacina vacina) => vacinaModel.Update(vacina);
+        public bool Delete(int vacinaID) => vacinaModel.Delete(vacinaID);
+        public List<Vacina> GetAll => vacinaModel.GetAll();
+        public Vacina Get(int vacinaID) => vacinaModel.Get(vacinaID);
     }
 }
