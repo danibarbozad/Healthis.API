551b803ea99a81e80ee38ba52101b5ff9a01a3a5
Daniel Barboza <danibarbozad@gmail.com>
2022-06-16 13:39:28 -0300

    create user service (without login temporarily)

--- Healthis.Service/UsuarioService.cs
+++ Healthis.Service/UsuarioService.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Healthis.Entities;
+using Healthis.Model;
 
 namespace Healthis.Service
 {
-    class UsuarioService
+    public class UsuarioService
     {
+        private UsuarioModel usuarioModel;
+        public UsuarioService(string connectionString)
+        {
+            usuarioModel = new UsuarioModel(connectionString);
+        }
+
+        public Usuario Create(Usuario usuario) => usuarioModel.Create(usuario);
+        public Usuario Update(Usuario usuario) => usuarioModel.Update(usuario);
+        public bool Delete(int usuarioID) => usuarioModel.Delete(usuarioID);
+        public List<Usuario> GetAll => usuarioModel.GetAll();
+        public Usuario Get(int usuarioID) => usuarioModel.Get(usuarioID);
     }
 }
